63422c05a7262640936413d3207ef7003637a9ef
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2023-11-27 21:40:23 +0100

    chore: Autopep

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ class Formula:
     def __init__(self):
         pass
 
-    def modus_ponens(self,a,b):
+    def modus_ponens(self, a, b):
         """Applies the Modus Ponens rule to two logical propositions
 
         Args:
@@ -20,7 +20,7 @@ class Formula:
             bool: True if the formula is constructed with Axiom 1, False otherwise
         """
         pass
-        
+
     def is_axiom2(self):
         """Checks if the given formula is constructed using Axiom 2
 
@@ -28,7 +28,7 @@ class Formula:
             bool: True if the formula is constructed with Axiom 2, False otherwise
         """
         pass
-        
+
     def is_axiom3(self):
         """Checks if the given formula is constructed using Axiom 3
 
@@ -36,12 +36,12 @@ class Formula:
             bool: True if the formula is constructed with Axiom 3, False otherwise
         """
         pass
-        
+
     def is_axiom(self):
         """???
         """
         pass
-        
+
     def is_equal(self, formula):
         """Checks if the given formula is equal to the formula
 
@@ -55,12 +55,12 @@ class Formula:
 
 
 class Variable(Formula):
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
-        
+
     def __str__(self):
         return self.to_string()
-    
+
     def to_string(self):
         """Returns the variable as a readable string
 
@@ -68,7 +68,7 @@ class Variable(Formula):
             str: variable
         """
         return self.name
-    
+
     def evaluate(self, d):
         """Returns the assignment of a variable
 
@@ -79,7 +79,6 @@ class Variable(Formula):
             bool: assigment for the variable true or false
         """
         return d[self.name]
-            
 
     def get_variables(self):
         """Return all variables in the formula
@@ -88,16 +87,16 @@ class Variable(Formula):
             Set[str]: The variables as a set
         """
         return {self.name}
-        
+
 
 class Implies(Formula):
-    def __init__(self,form1, form2):
+    def __init__(self, form1, form2):
         self.form1 = form1
         self.form2 = form2
-        
+
     def __str__(self):
         return self.to_string()
-    
+
     def to_string(self):
         """Returns the formula to the left of the implies arrow
 
@@ -105,7 +104,7 @@ class Implies(Formula):
             str: The formula on the left side of the implies arrow
         """
         return f"({self.form1} -> {self.form2})"
-        
+
     def get_left(self):
         """Returns the formula to the left of the implies arrow (->)
 
@@ -113,7 +112,7 @@ class Implies(Formula):
             Formula: The formula on the left side of the implies arrow
         """
         return self.form1
-        
+
     def get_right(self):
         """Returns the formula to the right oft the implies arrow (->)
 
@@ -121,7 +120,7 @@ class Implies(Formula):
             Formula: The formula on the right side of the implies arrow
         """
         return self.form2
-    
+
     def evaluate(self, d):
         """Evaluates the boolean result of a logical formula containing an implies (->) operation
 
@@ -131,10 +130,8 @@ class Implies(Formula):
         Returns:
             bool: The result of the formula. true or false
         """
-        return not self.form1.evaluate(d) or self.form2.evaluate(d) 
+        return not self.form1.evaluate(d) or self.form2.evaluate(d)
 
-        
-    
     def get_variables(self):
         """Return all variables in the formula
 
@@ -142,16 +139,15 @@ class Implies(Formula):
             Set[str]: The variables as a set
         """
         return self.form1.get_variables().union(self.form2.get_variables())
-        
-    
-                
+
+
 class Not(Formula):
     def __init__(self, form):
         self.form = form
-    
+
     def __str__(self):
         return self.to_string()
-    
+
     def to_string(self):
         """Returns the formula with an "not" symbole
 
@@ -159,7 +155,7 @@ class Not(Formula):
             str: The formula with an "not" symbole
         """
         return f"~({self.form})"
-    
+
     def get_form(self):
         """Returns the formula without the negation
 
@@ -167,7 +163,7 @@ class Not(Formula):
             Formula: The formula without negation
         """
         return self.form
-    
+
     def get_variables(self):
         """Return all variables in the formula
 
@@ -175,7 +171,7 @@ class Not(Formula):
             Set[str]: The variables as a set
         """
         return self.form.get_variables()
-    
+
     def evaluate(self, d):
         """Returns the negation of the given formula
 
@@ -186,24 +182,25 @@ class Not(Formula):
             bool: negation of the formula
         """
         return not self.form.evaluate(d)
-    
+
+
 class And(Formula):
-    def __init__(self,form1, form2):
+    def __init__(self, form1, form2):
         self.form1 = form1
         self.form2 = form2
-        
+
     def __str__(self):
         return self.to_string()
-    
+
     def to_string(self):
         return f"({self.form1} /\ {self.form2})"
-        
+
     def get_left(self):
         return self.form1
-        
+
     def get_right(self):
         return self.form2
-    
+
     def evaluate(self, d):
         """Evaluates the boolean result of a logical formula containing an And (/\) operation
 
@@ -223,23 +220,24 @@ class And(Formula):
         """
         return self.form1.get_variables().union(self.form2.get_variables())
 
+
 class Or(Formula):
-    def __init__(self,form1, form2):
+    def __init__(self, form1, form2):
         self.form1 = form1
         self.form2 = form2
-        
+
     def __str__(self):
         return self.to_string()
-    
+
     def to_string(self):
         return f"({self.form1} \/ {self.form2})"
-        
+
     def get_left(self):
         return self.form1
-        
+
     def get_right(self):
         return self.form2
-    
+
     def evaluate(self, d):
         """Evaluates the boolean result of a logical formula containing an Or (\/) operation
 
@@ -259,11 +257,12 @@ class Or(Formula):
         """
         return self.form1.get_variables().union(self.form2.get_variables())
 
+
 class Proof:
     def __init__(self, assumptions, proof):
         self.assumptions = assumptions
         self.proof = proof
-        
+
     def verify(self):
         """Returns an true if the proof is correct and an false if its not correct
 
@@ -277,17 +276,19 @@ def test_case_1():
     # This test case should return False
     a = Variable("a")
     b = Variable("b")
-    test = Proof([a,b],[a,b, Implies(a,b)])
+    test = Proof([a, b], [a, b, Implies(a, b)])
     return test.verify()
 
+
 def test_case_2():
-    # This test case should return True 
+    # This test case should return True
     a = Variable("a")
     b = Variable("b")
-    assumptions = [a,b]
-    proof = [a,b, Implies(a,Implies(b,a))]
-    test = Proof(assumptions,proof)
+    assumptions = [a, b]
+    proof = [a, b, Implies(a, Implies(b, a))]
+    test = Proof(assumptions, proof)
     return test.verify()
+
 
 print("Test case1: ", test_case_1())
 print("Test case2: ", test_case_2())
